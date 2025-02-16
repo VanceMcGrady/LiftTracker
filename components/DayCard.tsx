7c696c81a2ca781771de3611c3e7e79768b7c498
VanceMcGrady <vancemcgrady@gmail.com>
2025-02-16 16:28:58 -0700

    some style updates to the DayCard

--- components/DayCard.tsx
+++ components/DayCard.tsx
@@ -3,7 +3,17 @@ import { View, Text } from "react-native";
 
 function DayCard(props: { day: string }) {
   return (
-    <View>
+    <View
+      style={{
+        backgroundColor: "lightblue",
+        padding: 10,
+        borderWidth: 1,
+        borderColor: "black",
+        borderStyle: "solid",
+        borderRadius: 5,
+        width: "100%",
+      }}
+    >
       <Text>{props.day}</Text>
     </View>
   );
