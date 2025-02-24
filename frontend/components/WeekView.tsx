38138991134c3f3acb175f06cc88e71bce1275db
VanceMcGrady <vancemcgrady@gmail.com>
2025-02-24 11:04:56 -0700

    moved workout provider to WeekView

--- frontend/components/WeekView.tsx
+++ frontend/components/WeekView.tsx
@@ -3,6 +3,7 @@ import { View, Text } from "react-native";
 import DayCard from "./DayCard"; // Ensure this path is correct
 import { Link } from "expo-router";
 import { WorkoutContext } from "@/context/RoutineContext";
+import { WorkoutProvider } from "@/context/RoutineContext";
 
 function WeekView(props: any) {
   const { schedule } = React.useContext(WorkoutContext) as any;
@@ -16,27 +17,29 @@ function WeekView(props: any) {
   }
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        width: "100%",
-        gap: 10,
-        paddingHorizontal: 10,
-      }}
-    >
-      {schedule.map((routine: any) => (
-        <Link
-          href={{
-            pathname: `./workout/${routine.dayOfWeek}`,
-          }}
-          key={routine.id}
-        >
-          <DayCard key={routine.id} routine={routine} />
-        </Link>
-      ))}
-    </View>
+    <WorkoutProvider>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          gap: 10,
+          paddingHorizontal: 10,
+        }}
+      >
+        {schedule.map((routine: any) => (
+          <Link
+            href={{
+              pathname: `./workout/${routine.dayOfWeek}`,
+            }}
+            key={routine.id}
+          >
+            <DayCard key={routine.id} routine={routine} />
+          </Link>
+        ))}
+      </View>
+    </WorkoutProvider>
   );
 }
 
